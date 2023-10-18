6a7b00eb2d25de9cfca3ebdfd8446a5acec2e62d
danimanrey <145423431+danimanrey@users.noreply.github.com>
2023-10-18 17:40:06 +0200

    push

--- app.py
+++ app.py
@@ -20,3 +20,17 @@ plt.xlabel('Sex')
 plt.ylabel('Number of passengers')
 plt.title('Survival rate by sex')
 st.pyplot()
+
+'''
+Visualizamos la variable Embarked
+'''
+survived_embark =
+train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=False)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False)
+embark_df =
+pd.DataFrame([survived_embark,dead_embark],index=['Survived','Dead'])
+embark_df.plot(kind='bar', stacked=True)
+plt.xlabel('Embarked')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by port of embarkation')
+st.pyplot()
