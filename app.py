4df01482a5a37b92b8b8db80c3091291de8feb34
KinoGalvez <joaquingr1969@gmail.com>
2023-10-18 17:02:42 +0200

    branch_ticket_k3

--- app.py
+++ app.py
@@ -16,3 +16,13 @@ y = train_df[[Survived']]
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 model = RandomForestClassifier(n_estimators=100, random_state=0)
 model.fit(X_train, y_train)
+# Developer 3 - Parte 3
+'''Hacemos predicciones'''
+model.fit(X_train, y_val)
+y_pred = model.predict(X_val)
+accuracy = accuracy_score(y_val, y_pred)
+importances =
+pd.DataFrame({'feature':X_train.columns,'importance':np.round(model.feature_importan
+ces_,3)})
+importances =
+importances.sort_values('importance',ascending=False).set_index('feature')
