9fc5e58872371d5f62d1166b9c33f6bdf9d85e5c
hmsgrs <hmartinsantos@gmail.com>
2023-10-18 16:07:27 +0200

    branch_ticket_2

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+'''
+Importamos librerías
+'''
+import pandas
+import numpy
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+st.set_option('deprecation.showPyplotGlobalUse', False)
