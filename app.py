ac35ec7aca3dbd91fa8f7451cd47b68310f965ff
StevenNoboa <1bstevennoboa@gmail.com>
2023-10-18 16:06:05 +0200

    D2 P1

---
+++ app.py
@@ -0,0 +1,4 @@
+train_df['Sex'] = train_df['Sex'].map({'male': 0, 'female': 1})
+test_df['Sex'] = test_df['Sex'].map({'male': 0, 'female': 1})
+train_df['Sex'].fillna(train_df['Age'].mean(), inplace=True)
+test_df['Sex'].fillna(test_df['Age'].mean(), inplace=True)
