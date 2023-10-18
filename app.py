a1181a134701335e86631b1882e46b432d10be2e
danimanrey <145423431+danimanrey@users.noreply.github.com>
2023-10-18 17:44:37 +0200

    push

--- app.py
+++ app.py
@@ -34,3 +34,13 @@ plt.xlabel('Embarked')
 plt.ylabel('Number of passengers')
 plt.title('Survival rate by port of embarkation')
 st.pyplot()
+
+'''
+Visualizamos la importancia de las variables
+'''
+survived_embark =
+train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=True)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False)
+st.write('Visualización del modelo y datos')
+st.write('Importancia de características en el modelo')
+st.bar_chart(importances)
