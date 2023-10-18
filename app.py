284224ba6948140531c1b65e98cc82cca70b3ab1
hmsgrs <hmartinsantos@gmail.com>
2023-10-18 16:19:24 +0200

    ticket3

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 st.set_option('deprecation.showPyplotGlobalUse', False)
+'''
+Cargamos los datos
+'''
+train_df = pd.read_csv('data/train.csv')
+test_df = pd.read_csv('data/test.csv')
